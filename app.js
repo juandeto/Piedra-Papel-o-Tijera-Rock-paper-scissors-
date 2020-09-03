c9ae29113fe6ca97bea2d49de059b464b4803deb
juandeto <juandeto10@gmail.com>
2020-09-03 10:15:36 -0300

    changes in style

--- app.js
+++ app.js
@@ -8,20 +8,21 @@ var nombrePlayer = document.getElementById('nombrePlayer');
 var imagenPc = document.getElementById('imagenPc');
 var imagenPlayer = document.getElementById('imagenPlayer');
 
-var resultadoTotal = document.getElementById('resultado-total');
+var resultadoTotal=document.getElementsByClassName('score');
 var pcArray = [];
 var pc = '';
 var player = '';
 var resultado = document.getElementById('resultado');
 seconds = 3;
-resultadoTotal.textContent = '';
+resultadoTotal[0].textContent = 0;
+resultadoTotal[1].textContent = 0;
 
 window.onload = function () {
     resultado.innerText = '¡Selecciona un elemento!';
     nombrePc.textContent = '';
     nombrePlayer.textContent = '';
-    imagenPc.innerHTML = '<img src="PIEDRA.png"alt="foto" class="imagenMano">';
-    imagenPlayer.innerHTML = '<img src="PIEDRA.png" alt="" class="imagenMano">';
+    imagenPc.innerHTML = '<img src="PIEDRA-removebg-preview.png"alt="foto" class="imagenMano">';
+    imagenPlayer.innerHTML = '<img src="PIEDRA-removebg-preview.png" alt="" class="imagenMano">';
 }
 
 
@@ -33,12 +34,11 @@ for (let i = 0; i < btn.length; i++) {
 btn.forEach(btn => btn.addEventListener('click', startTimer))
 
 function limpiarTablero() {
-    resultado.style.color = 'black';
     resultado.innerText = '';
     nombrePc.textContent = '';
     nombrePlayer.textContent = '';
-    imagenPc.innerHTML = '<img src="PIEDRA.png"alt="foto" class="imagenMano">';
-    imagenPlayer.innerHTML = '<img src="PIEDRA.png" alt="" class="imagenMano">';
+    imagenPc.innerHTML = '<img src="PIEDRA-removebg-preview.png"alt="foto" class="imagenMano">';
+    imagenPlayer.innerHTML = '<img src="PIEDRA-removebg-preview.png" alt="" class="imagenMano">';
 
 
 }
@@ -79,30 +79,35 @@ function randomNum() {
 
 var counterPC = 0;
 var counterPlayer = 0;
-resultadoTotal.textContent = `${'PC: ' + counterPC + '    '}${'Player: ' + counterPlayer}`;
+resultadoTotal[0].textContent = counterPC ;
+resultadoTotal[1].textContent =counterPlayer;
 
 function ganador() {
     console.log(resultado);
     nombrePc.textContent = `${'PC: ' + pc}`;
     nombrePlayer.textContent = `${'Jugador: ' + player}`;
     console.log(resultado.innerText);
-    imagenPc.innerHTML = '<img src="' + `${pc + '.png'}` + '"alt="foto" class="imagenMano">';
-    imagenPlayer.innerHTML = '<img src="' + `${player + '.png'}` + '" alt="" class="imagenMano">';
+    imagenPc.innerHTML = '<img src="' + `${pc + '-removebg-preview.png'}` + '"alt="foto" class="imagenMano">';
+    imagenPlayer.innerHTML = '<img src="' + `${player + '-removebg-preview.png'}` + '" alt="" class="imagenMano">';
 
     if (player == pc) {
         resultado.textContent = "¡Empate! juga de nuevo";
-        resultado.style.color = 'green';
+        
     } else if (pc == 'PIEDRA' && player == 'PAPEL') {
         ganaste();
+        
     } else if (pc == 'PAPEL' && player == 'TIJERA') {
         ganaste();
+        
     } else if (pc == 'TIJERA' && player == 'PIEDRA') {
         ganaste();
+        
     } else {
         perdiste();
+        
     }
-    resultadoTotal.textContent = `${'PC: ' + counterPC + '    '}${'Player: ' + counterPlayer}`;
-
+    resultadoTotal[0].textContent = counterPC ;
+    resultadoTotal[1].textContent =counterPlayer;
 
     if (counterPC == 3 || counterPlayer == 3) {
         btn.forEach(btn => btn.removeEventListener('click', startTimer));
@@ -112,13 +117,11 @@ function ganador() {
 }
 function ganaste() {
     resultado.innerText = "¡GANASTE!";
-    resultado.style.color = 'gold';
     counterPlayer += 1;
 }
 
 function perdiste() {
     resultado.innerText = "PERDISTE... :(";
-    resultado.style.color = 'red';
     counterPC += 1;
 }
 
@@ -133,7 +136,7 @@ function finJuego() {
 
             })
         } else if (counterPC == 3) {
-            pantalla.innerHTML = '<h1 style="color: red"> PER-DIS-TE! </h1> <br> <span class="jugarDeNuevo">Jugar de nuevo</span>';
+            pantalla.innerHTML = '<h1 style="color: darkred"> PER-DIS-TE! </h1> <br> <span class="jugarDeNuevo">Jugar de nuevo</span>';
             var reiniciar = document.getElementsByClassName('jugarDeNuevo')[0];
             reiniciar.addEventListener('click', function () {
                 location.reload();
